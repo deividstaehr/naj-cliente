346ea0845ea4330de055c647771481663ad5bfaa
RobertoKlann <robertoklann17@gmail.com>
2020-12-04 11:38:30 -0300

    Ajuste CHAT

--- resources/views/areaCliente/mensagens.blade.php
+++ resources/views/areaCliente/mensagens.blade.php
@@ -81,7 +81,7 @@
             <div class="btn-group dropup show pl-0 content-input-mensagem-chat" id="content-input-tres-pontos-chat" style="width: 3% !important; left: 17px; position: absolute; background-color: #f1f1f1 !important;">
                 <button type="button" class="btn btn-light btn-light-atendimento-dropdown" id="input-anexo"><i class="fas fa-paperclip"></i></button>
             </div>
-            <textarea name="" id="input-text-chat-enviar" class="input-mensagem-chat content-input-mensagem-chat" wrap="hard" placeholder="Digete sua mensagem"></textarea>
+            <textarea name="" id="input-text-chat-enviar" class="input-mensagem-chat content-input-mensagem-chat" wrap="hard" placeholder="Digite sua mensagem"></textarea>
             <div class="content-button-enviar-smartphone">
                 <button type="button" class="btn btn-success btn-circle" id="button-enviar-smartphone"><i class="fas fa-paper-plane" style="margin-left: -5px;"></i></button>
             </div>
