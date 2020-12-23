8497338932906ea168295834323c00df2ca671b9
RobertoKlann <robertoklann17@gmail.com>
2020-12-23 17:40:29 -0300

    Ajustes modo DOWNLOAD

--- public/js/atividades.js
+++ public/js/atividades.js
@@ -214,26 +214,44 @@ async function onClickDownloadAnexoAtividade(codigo, arquivoName) {
     let identificador = sessionStorage.getItem('@NAJ_CLIENTE/identificadorEmpresa');
     let parametros    = JSON.stringify({codigo, identificador, 'original_name' : arquivoName});
     let result        = await NajApi.getData(`atividade/download/${parametros}?XDEBUG_SESSION_START`, true);
-    let name          = arquivoName.split('.')[0];
-    let ext           = arquivoName.split('.')[1];
 
     if(result && result.size > 0) {
-        var element = document.createElement('a');
-        var reader  = new FileReader();
-
-        reader.onloadend = function () {
-            element.setAttribute('href', reader.result);
-            element.setAttribute('download', `${name}.${ext}`);
-            element.style = 'none';
-            document.body.appendChild(element);
-            element.click();
-            document.body.removeChild(element);
-        }
-
-        reader.readAsDataURL(result);
+        const url = URL.createObjectURL(result);
+  
+        // Create a new anchor element
+        const a = document.createElement('a');
+        
+        // Set the href and download attributes for the anchor element
+        // You can optionally set other attributes like `title`, etc
+        // Especially, if the anchor element will be attached to the DOM
+        a.href = url;
+        a.download = arquivoName || 'download';
+        
+        // Click handler that releases the object URL after the element has been clicked
+        // This is required for one-off downloads of the blob content
+        const clickHandler = () => {
+            setTimeout(() => {
+                URL.revokeObjectURL(url);
+                this.removeEventListener('click', clickHandler);
+            }, 150);
+        };
+        
+        // Add the click event listener on the anchor element
+        // Comment out this line if you don't want a one-off download of the blob content
+        a.addEventListener('click', clickHandler, false);
+        
+        // Programmatically trigger a click on the anchor element
+        // Useful if you want the download to happen automatically
+        // Without attaching the anchor element to the DOM
+        // Comment out this line if you don't want an automatic download of the blob content
+        a.click();
+        
+        // Return the anchor element
+        // Useful if you want a reference to the element
+        // in order to attach it to the DOM or use it in some other way
+        loadingDestroy('loading-download-anexo-atividade');
     } else {
         NajAlert.toastError('Não foi possível fazer o download, o anexo não foi encontrado!');
+        loadingDestroy('loading-download-anexo-atividade');
     }
-
-    loadingDestroy('loading-download-anexo-atividade');
 }
