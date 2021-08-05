64b8ca104ad234395a085f2b0e4133d2de780039
RobertoKlann <robertoklann17@gmail.com>
2021-08-04 22:14:57 -0300

    Ajustes layout agenda

--- resources/views/areaCliente/consulta/AgendaConsultaView.blade.php
+++ resources/views/areaCliente/consulta/AgendaConsultaView.blade.php
@@ -4,22 +4,122 @@
 
 @section('css')
     <link rel="stylesheet" href="{{ env('APP_URL') }}css/gijgo.min.css">
-    <style>
-        button {
-            background: #fff !important;
-        }
-        .naj-datatable i {
-            color: rgba(47, 50, 62, .75);
-            cursor: pointer;
-            font-size: 14px;
-        }
-    </style>
+    <link rel="stylesheet" href="{{ env('APP_URL') }}css/agenda.css">
 @endsection
 
 @section('active-layer', 'agendaCompromissos')
 @section('content')
 
-<div id="datatable-agenda" class="naj-datatable" style="height: 100%;"></div>
+<!-- <div id="datatable-agenda" class="naj-datatable" style="height: 100%;"></div> -->
+
+<div class="content-agenda">
+    <div class="row" style="height: 100%;">
+        <div class="col-8" style="height: 100%;">
+            <div class="container-agenda naj-scrollable">
+                <div class="row row-striped">
+                    <div class="col-2 text-right">
+                        <h3 class="display-4"><span class="badge badge-info">23</span></h3>
+                        <h4>Agosto</h4>
+                    </div>
+                    <div class="col-10">
+                        <h5 class="text-uppercase"><strong>Enviar relatório atendimentos ADM</strong></h5>
+                        <ul class="list-inline">
+                            <li class="list-inline-item"><i class="fa fa-calendar-o" aria-hidden="true"></i> Segunda</li>
+                            <li class="list-inline-item"><i class="fa fa-clock-o" aria-hidden="true"></i> 12:30 PM - 2:00 PM</li>
+                            <li class="list-inline-item"><i class="fa fa-location-arrow" aria-hidden="true"></i> Local do Evento</li>
+                        </ul>
+                        <p>Aqui vai qualqer que seja a descrição do evento apenas para ilustrar mesmo.</p>
+                    </div>
+                </div>
+                <div class="row row-striped">
+                    <div class="col-2 text-right">
+                        <h3 class="display-4"><span class="badge badge-info">24</span></h3>
+                        <h4>Agosto</h4>
+                    </div>
+                    <div class="col-10">
+                        <h5 class="text-uppercase"><strong>Enviar relatório atendimentos ADM</strong></h5>
+                        <ul class="list-inline">
+                            <li class="list-inline-item"><i class="fa fa-calendar-o" aria-hidden="true"></i> Segunda</li>
+                            <li class="list-inline-item"><i class="fa fa-clock-o" aria-hidden="true"></i> 12:30 PM - 2:00 PM</li>
+                            <li class="list-inline-item"><i class="fa fa-location-arrow" aria-hidden="true"></i> Local do Evento</li>
+                        </ul>
+                        <p>Aqui vai qualqer que seja a descrição do evento apenas para ilustrar mesmo.</p>
+                    </div>
+                </div>
+                <div class="row row-striped">
+                    <div class="col-2 text-right">
+                        <h3 class="display-4"><span class="badge badge-info">25</span></h3>
+                        <h4>Agosto</h4>
+                    </div>
+                    <div class="col-10">
+                        <h5 class="text-uppercase"><strong>Cliente: Cintia Feliski- Atendimento</strong></h5>
+                        <ul class="list-inline">
+                            <li class="list-inline-item"><i class="fa fa-calendar-o" aria-hidden="true"></i> Segunda</li>
+                            <li class="list-inline-item"><i class="fa fa-clock-o" aria-hidden="true"></i> 12:30 PM - 2:00 PM</li>
+                            <li class="list-inline-item"><i class="fa fa-location-arrow" aria-hidden="true"></i> Local do Evento</li>
+                        </ul>
+                        <p>Aqui vai qualqer que seja a descrição do evento apenas para ilustrar mesmo.</p>
+                    </div>
+                </div>
+                <div class="row row-striped">
+                    <div class="col-2 text-right">
+                        <h3 class="display-4"><span class="badge badge-info">26</span></h3>
+                        <h4>Agosto</h4>
+                    </div>
+                    <div class="col-10">
+                        <h5 class="text-uppercase"><strong>Atendimento Sra Desireé</strong></h5>
+                        <ul class="list-inline">
+                            <li class="list-inline-item"><i class="fa fa-calendar-o" aria-hidden="true"></i> Segunda</li>
+                            <li class="list-inline-item"><i class="fa fa-clock-o" aria-hidden="true"></i> 12:30 PM - 2:00 PM</li>
+                            <li class="list-inline-item"><i class="fa fa-location-arrow" aria-hidden="true"></i> Local do Evento</li>
+                        </ul>
+                        <p>Aqui vai qualqer que seja a descrição do evento apenas para ilustrar mesmo.</p>
+                    </div>
+                </div>
+                <div class="row row-striped">
+                    <div class="col-2 text-right">
+                        <h1 class="display-4"><span class="badge badge-info">28</span></h1>
+                        <h4>Agosto</h4>
+                    </div>
+                    <div class="col-10">
+                        <h5 class="text-uppercase"><strong>Reunião</strong></h5>
+                        <ul class="list-inline">
+                            <li class="list-inline-item"><i class="fa fa-calendar-o" aria-hidden="true"></i> Sexta</li>
+                            <li class="list-inline-item"><i class="fa fa-clock-o" aria-hidden="true"></i> 2:30 PM - 4:00 PM</li>
+                            <li class="list-inline-item"><i class="fa fa-location-arrow" aria-hidden="true"></i> Local do Evento</li>
+                        </ul>
+                        <p>Aqui vai qualqer que seja a descrição do evento apenas para ilustrar mesmo.</p>
+                    </div>
+                </div>
+                <div class="row row-striped">
+                    <div class="col-2 text-right">
+                        <h1 class="display-4"><span class="badge badge-info">30</span></h1>
+                        <h4>Agosto</h4>
+                    </div>
+                    <div class="col-10">
+                        <h5 class="text-uppercase"><strong>Reunião com sra. Aline</strong></h5>
+                        <ul class="list-inline">
+                            <li class="list-inline-item"><i class="fa fa-calendar-o" aria-hidden="true"></i> Sexta</li>
+                            <li class="list-inline-item"><i class="fa fa-clock-o" aria-hidden="true"></i> 2:30 PM - 4:00 PM</li>
+                            <li class="list-inline-item"><i class="fa fa-location-arrow" aria-hidden="true"></i> Local do Evento</li>
+                        </ul>
+                        <p>Aqui vai qualqer que seja a descrição do evento apenas para ilustrar mesmo.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-4" style="margin-top: 6%;">
+            <h5 class="mt-4 weight-700 text-center">Escolha uma das opções de AGENDAMENTOS abaixo:</h5>
+            <div class="content-agenda-buttons">                    
+                <button class="btn-agenda btn btn-primary" onclick="onClickAgendarConsulta()">Agendar uma Consulta</button>
+                <button class="btn-agenda btn btn-info" onclick="onClickAgendarReuniao()">Agendar uma Reunião</button>
+                <button class="btn-agenda btn btn-warning" onclick="onClickAgendarVisita()">Agendar uma Visita</button>
+                <button class="btn-agenda btn btn-secondary" onclick="onClickOutroAgendamento()">Outro tipo de Agendamento</button>
+            </div>
+        </div>
+    </div>
+</div>
 
 @component('areaCliente.componentes.modalConsultaAnexoAtividade')
 @endcomponent
