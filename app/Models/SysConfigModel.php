b233041f8c5761e447743100ff0d043022afa8ee
RobertoKlann <robertoklann17@gmail.com>
2021-08-09 20:38:20 -0300

    Ajustado agenda

--- app/Models/SysConfigModel.php
+++ app/Models/SysConfigModel.php
@@ -58,9 +58,92 @@ class SysConfigModel extends NajModel {
      * @return StdClass
      */
     public function searchSysConfig($secao, $chave) {
-        return  DB::table('sys_config')
-                ->where(['SECAO' => $secao, 'CHAVE' => $chave])
-                ->limit(1)
-                ->first()->VALOR;
+        $data = DB::table('sys_config')
+            ->where(['SECAO' => $secao, 'CHAVE' => $chave])
+            ->limit(1)
+            ->first();
+
+        if ($data)
+            return $data->VALOR;
+
+        return false;
     }
+
+
+ // /**
+    //  * Verifica se Empresa existe
+    //  * 
+    //  * @param array $data
+    //  * @return StdClass
+    //  */
+    // public function existsEmpresa($data) {
+    //     return DB::select("
+    //         SELECT *
+    //           FROM sys_config
+    //          WHERE secao = '{$data['SECAO']}'
+    //            AND chave = '{$data['CHAVE']}'
+    //     ");
+    // }
+     // /**
+    //  * Verifica se Empresa existe
+    //  * 
+    //  * @param array $data
+    //  * @return StdClass
+    //  */
+    // public function existsEmpresa($data) {
+    //     return DB::select("
+    //         SELECT *
+    //           FROM sys_config
+    //          WHERE secao = '{$data['SECAO']}'
+    //            AND chave = '{$data['CHAVE']}'
+    //     ");
+    // }
+
+
+
+
+    // /**
+    //  * Verifica se Empresa existe
+    //  * 
+    //  * @param array $data
+    //  * @return StdClass
+    //  */
+    // public function existsEmpresa($data) {
+    //     return DB::select("
+    //         SELECT *
+    //           FROM sys_config
+    //          WHERE secao = '{$data['SECAO']}'
+    //            AND chave = '{$data['CHAVE']}'
+    //     ");
+    // }
+
+    // /**
+    //  * Deleta registro no Sys_Config
+    //  * 
+    //  * @param string $secao
+    //  * @param string $chave
+    //  * @return int
+    //  */
+    // public function destroySysConfig($secao, $chave) {
+    //     return DB::table('sys_config')->where(['SECAO' => $secao, 'CHAVE' => $chave])->delete();
+    // }
+    
+    // /**
+    //  * Busca registro no Sys_Config
+    //  * 
+    //  * @param string $secao
+    //  * @param string $chave
+    //  * @return StdClass
+    //  */
+    // public function searchSysConfig($secao, $chave) {
+    //     $data = DB::table('sys_config')
+    //         ->where(['SECAO' => $secao, 'CHAVE' => $chave])
+    //         ->limit(1)
+    //         ->first();
+
+    //     if ($data)
+    //         return $data->VALOR;
+
+    //     return false;
+    // }
 }
